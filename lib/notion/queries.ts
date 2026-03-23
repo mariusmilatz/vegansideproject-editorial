304e5e458b41a7583a1ecd92d366a95ed57a91ac
Marius <41435955+mariusmilatz@users.noreply.github.com>
2026-03-23 10:31:54 +0100

    Restore notion queries file

---
+++ lib/notion/queries.ts
@@ -0,0 +1,20 @@
+import { getNotionClient } from "./client"
+
+export async function getRecipes(): Promise<any[]> {
+  const notion = getNotionClient()
+  const databaseId = process.env.NOTION_RECIPES_DATABASE_ID
+
+  if (!databaseId) {
+    throw new Error("Missing NOTION_RECIPES_DATABASE_ID")
+  }
+
+  const response = await notion.databases.query({
+    database_id: databaseId,
+    filter: {
+      property: "Published",
+      checkbox: { equals: true }
+    }
+  })
+
+  return response.results as any[]
+}
